26fbabb8111453dcf05bdff2ec1114df96e6aa9f
Cynderian <thispersonlikespie@gmail.com>
2025-02-03 11:11:50 -0600

    profile pic and bio

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,9 +1,11 @@
 import styles from "./App.module.css";
 import { Navbar } from "./components/navbar/Navbar";
+import { ProfilePic } from "./components/ProfilePic/ProfilePic";
 
 function App() {
   return <div className={styles.App}>
     <Navbar />
+    <ProfilePic />
   </div>;
 }
 
---
+++ react-portfolio/src/components/ProfilePic/ProfilePic.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { getImageUrl } from "../../utils";
+import styles from "./ProfilePic.module.css";
+
+export const ProfilePic = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Hi, I'm Vivian</h1>
+                <p className={styles.description}>I'm a full stack developer with 3 years of experience in using React and NodeJS. Reach out if you'd like to learn more!</p>
+                <a href="mailto:viviantucholski@gmail.com" classname={styles.contactLink}>Contact Me</a>
+            </div>
+            <img src={ getImageUrl("vvttmp.jpg")} alt="Profile Picture" className={styles.profileImg}/>
+            <div className={styles.topBlur} />
+            <div className={styles.bottomBlur} />
+        </section>
+    )
+}
