32bdcbc10fcfa2a468b889b4c43ea2f730b00906
Cynderian <thispersonlikespie@gmail.com>
2025-02-03 11:26:16 -0600

    apostrophes escaped

--- react-portfolio/src/components/ProfilePic/ProfilePic.jsx
+++ react-portfolio/src/components/ProfilePic/ProfilePic.jsx
@@ -7,8 +7,8 @@ export const ProfilePic = () => {
     return (
         <section className={styles.container}>
             <div className={styles.content}>
-                <h1 className={styles.title}>Hi, I'm Vivian</h1>
-                <p className={styles.description}>I'm a full stack developer with 3 years of experience in using React and NodeJS. Reach out if you'd like to learn more!</p>
+                <h1 className={styles.title}>Hi, I&apos;m Vivian</h1>
+                <p className={styles.description}>I&apos;m a full stack developer with 3 years of experience in using React and NodeJS. Reach out if you&apos;d like to learn more!</p>
                 <a href="mailto:viviantucholski@gmail.com" className={styles.contactLink}>Contact Me</a>
             </div>
             <img src={ getImageUrl("vvttmp.jpg")} alt="Profile Picture" className={styles.profileImg}/>
