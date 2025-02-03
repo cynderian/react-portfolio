6f0f6e581ed576a0edd23f583a2367a8fd31f6cb
Cynderian <thispersonlikespie@gmail.com>
2025-02-03 14:48:36 -0600

    basic about section

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import styles from "./App.module.css";
 import { Navbar } from "./components/navbar/Navbar";
 import { ProfilePic } from "./components/ProfilePic/ProfilePic";
+import { About } from "./components/About/About"
 
 function App() {
   return <div className={styles.App}>
     <Navbar />
     <ProfilePic />
+    <About />
   </div>;
 }
 
