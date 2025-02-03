adc0c0de48025413856bd70b5c33fc4137db47a7
Cynderian <thispersonlikespie@gmail.com>
2025-02-03 11:20:22 -0600

    contact css linked

--- react-portfolio/src/components/ProfilePic/ProfilePic.jsx
+++ react-portfolio/src/components/ProfilePic/ProfilePic.jsx
@@ -9,7 +9,7 @@ export const ProfilePic = () => {
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I'm Vivian</h1>
                 <p className={styles.description}>I'm a full stack developer with 3 years of experience in using React and NodeJS. Reach out if you'd like to learn more!</p>
-                <a href="mailto:viviantucholski@gmail.com" classname={styles.contactLink}>Contact Me</a>
+                <a href="mailto:viviantucholski@gmail.com" className={styles.contactLink}>Contact Me</a>
             </div>
             <img src={ getImageUrl("vvttmp.jpg")} alt="Profile Picture" className={styles.profileImg}/>
             <div className={styles.topBlur} />
