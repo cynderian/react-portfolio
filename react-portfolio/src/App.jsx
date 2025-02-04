c07882b87f117fe4591cbf19a4ae70ee17a846e3
Cynderian <thispersonlikespie@gmail.com>
2025-02-04 13:20:47 -0600

    experience section

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -2,12 +2,14 @@ import styles from "./App.module.css";
 import { Navbar } from "./components/navbar/Navbar";
 import { ProfilePic } from "./components/ProfilePic/ProfilePic";
 import { About } from "./components/About/About"
+import { Experience } from "./components/Experience/Experience";
 
 function App() {
   return <div className={styles.App}>
     <Navbar />
     <ProfilePic />
     <About />
+    <Experience />
   </div>;
 }
 
---
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import styles from "./Experience.module.css";
+import skills from "../../data/skills.json"
+import history from "../../data/history.json"
+import { getImageUrl } from "../../utils";
+
+export const Experience = () => {
+    return (
+        <section id="experience">
+        <h2>Experience</h2>
+        <div>
+            <div>
+                {skills.map((skill, id) => {
+                    return (
+                        <div key={id}>
+                            <div>
+                                <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                            </div>
+                            <p>{skill.title}</p>
+                        </div>
+                    );
+                })}
+            </div>
+            <ul>
+                {
+                    history.map((historyItem, id) => {
+                        return (<li key={id}>
+                            <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organization} Logo`}/>
+                            <div>
+                                <h3>{`${historyItem.role}, ${historyItem.organization}`}</h3>
+                                <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                                <ul>{historyItem.description.map((experience, id) => {
+                                    return <li key={id}>{experience}</li>;
+                                })}
+                                </ul>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+        </section>
+    )
+}
