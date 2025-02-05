e7d5c86c105fba4795781e14b18f3f82fb606cdd
Cynderian <thispersonlikespie@gmail.com>
2025-02-04 18:02:30 -0600

    experience css

--- react-portfolio/src/components/Experience/Experience.jsx
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -7,14 +7,14 @@ import { getImageUrl } from "../../utils";
 
 export const Experience = () => {
     return (
-        <section id="experience">
-        <h2>Experience</h2>
-        <div>
-            <div>
+        <section className={styles.container} id="experience">
+        <h2 className={styles.title}>Experience</h2>
+        <div className={styles.content}>
+            <div className={styles.skills}>
                 {skills.map((skill, id) => {
                     return (
-                        <div key={id}>
-                            <div>
+                        <div key={id} className={styles.skill}>
+                            <div className={styles.skillImageContainer}>
                                 <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
                             </div>
                             <p>{skill.title}</p>
@@ -22,21 +22,22 @@ export const Experience = () => {
                     );
                 })}
             </div>
-            <ul>
+            <ul className={styles.history}>
                 {
                     history.map((historyItem, id) => {
-                        return (<li key={id}>
-                            <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organization} Logo`}/>
-                            <div>
-                                <h3>{`${historyItem.role}, ${historyItem.organization}`}</h3>
-                                <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
-                                <ul>
-                                    {historyItem.description.map((description, id) => {
-                                        return <li key={id}>{description}</li>
-                                    })}
-                                </ul>
-                            </div>
-                        </li>
+                        return (
+                            <li key={id} className={styles.historyItem}>
+                                <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organization} Logo`}/>
+                                <div className={styles.historyItemDetails}>
+                                    <h3>{`${historyItem.role}, ${historyItem.organization}`}</h3>
+                                    <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                                    <ul>
+                                        {historyItem.description.map((description, id) => {
+                                            return <li key={id}>{description}</li>
+                                        })}
+                                    </ul>
+                                </div>
+                            </li>
                     );
                 })}
             </ul>
