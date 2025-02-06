8fa9de0f2bea72e7c07652c81891d9d175556d0a
Cynderian <thispersonlikespie@gmail.com>
2025-02-05 23:32:38 -0600

    projects start

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,8 +1,9 @@
 import styles from "./App.module.css";
 import { Navbar } from "./components/navbar/Navbar";
 import { ProfilePic } from "./components/ProfilePic/ProfilePic";
-import { About } from "./components/About/About"
+import { About } from "./components/About/About";
 import { Experience } from "./components/Experience/Experience";
+import { Projects } from "./components/Projects/Projects";
 
 function App() {
   return <div className={styles.App}>
@@ -10,6 +11,7 @@ function App() {
     <ProfilePic />
     <About />
     <Experience />
+    <Projects />
   </div>;
 }
 
---
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./Projects.module.css";
+
+export const Projects = () => {
+    return 1;
+}
