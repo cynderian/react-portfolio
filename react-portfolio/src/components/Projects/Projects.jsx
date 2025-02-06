99a07afddb7635a7689dad7c894ef745919af6e4
Cynderian <thispersonlikespie@gmail.com>
2025-02-06 15:38:57 -0600

    projects setup

--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 import styles from "./Projects.module.css";
+import { ProjectCard } from "./ProjectCard.jsx";
+import projects from "../../data/projects.json";
 
 export const Projects = () => {
-    return 1;
-}
+    return (
+        <section>
+            <h2>Projects</h2>
+            <div>
+                {
+                    projects.map((project, id) => {
+                        return <ProjectCard key={id} project={project}/>
+                    })
+                }
+            </div>
+        </section>
+    )
+};
