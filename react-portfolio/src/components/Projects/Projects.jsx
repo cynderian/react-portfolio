d3e265dfad4aa0517b10ba64212e2c3f10583ba5
Cynderian <thispersonlikespie@gmail.com>
2025-02-06 16:03:32 -0600

    css

--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -6,9 +6,9 @@ import projects from "../../data/projects.json";
 
 export const Projects = () => {
     return (
-        <section>
-            <h2>Projects</h2>
-            <div>
+        <section className={styles.container}>
+            <h2 className={styles.title}>Projects</h2>
+            <div className={styles.projects}>
                 {
                     projects.map((project, id) => {
                         return <ProjectCard key={id} project={project}/>
