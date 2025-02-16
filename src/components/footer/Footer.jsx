fbb43e01b203b6062bbb477d1c4f962f4af27b69
Cynderian <thispersonlikespie@gmail.com>
2025-02-15 22:12:31 -0600

    Style and layout improvements

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,17 +5,25 @@ export default function Footer() {
     return (
         <>
           <div className={styles.footer}>
-            <div>
-              <h1 className={styles.title}>Links</h1>
-              <a className={styles.link} href="https://www.linkedin.com/in/vivian-tucholski-19226b2b9/">LinkedIn</a>
-              <a className={styles.link} href="https://github.com/cynderian">GitHub</a>
-              <a>Resume</a>
-            </div>
-            <div>
-              <h1 className={styles.title}>Contact</h1>
-              <h2>vivian.tucholski@gmail.com</h2>
-              <h2>678-772-8938</h2>
-              <h2>Richardson, TX</h2>
+            <div className={styles.content}>
+              <div className={styles.linkBox}>
+                <h1 className={styles.title}>Links</h1>
+                <h2>
+                  <a href="https://www.linkedin.com/in/vivian-tucholski-19226b2b9/">LinkedIn</a>
+                </h2>
+                <h2>
+                  <a href="https://github.com/cynderian">GitHub</a>
+                </h2>
+                <h2>
+                  <a>Resume</a>
+                </h2>
+              </div>
+              <div className={styles.contactBox}>
+                <h1 className={styles.title}>Contact</h1>
+                <h2>vivian.tucholski@gmail.com</h2>
+                <h2>678-772-8938</h2>
+                <h2>Richardson, TX</h2>
+              </div>
             </div>
           </div>
       </>
