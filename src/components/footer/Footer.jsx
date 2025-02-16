7eb13c76de9d594a231fe97cea74a1b3fdbf107e
Cynderian <thispersonlikespie@gmail.com>
2025-02-15 22:44:13 -0600

    Added rough draft of footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,21 +8,21 @@ export default function Footer() {
             <div className={styles.content}>
               <div className={styles.linkBox}>
                 <h1 className={styles.title}>Links</h1>
-                <h2>
+                <p>
                   <a href="https://www.linkedin.com/in/vivian-tucholski-19226b2b9/">LinkedIn</a>
-                </h2>
-                <h2>
+                </p>
+                <p>
                   <a href="https://github.com/cynderian">GitHub</a>
-                </h2>
-                <h2>
+                </p>
+                <p>
                   <a>Resume</a>
-                </h2>
+                </p>
               </div>
               <div className={styles.contactBox}>
                 <h1 className={styles.title}>Contact</h1>
-                <h2>vivian.tucholski@gmail.com</h2>
-                <h2>678-772-8938</h2>
-                <h2>Richardson, TX</h2>
+                <p>vivian.tucholski@gmail.com</p>
+                <p>678-772-8938</p>
+                <p>Richardson, TX</p>
               </div>
             </div>
           </div>
