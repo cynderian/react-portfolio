0927ee7d69639b1370cb4b9e3854369b0af8ceec
Cynderian <thispersonlikespie@gmail.com>
2025-02-16 13:46:25 -0600

    More basic setup, spacing, sectioning

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,34 @@ import styles from "./Home.module.css";
 export default function Home() {
   return (
     <div className="home">
-      <h1 className={styles.title}>Hi, my name is Vivian Tucholski.</h1>
+      <div className={styles.intro}>
+        <h1 className={styles.title}>Hi, my name is Vivian Tucholski.</h1>
+        <p className={styles.about}>this is going to be a little section about myself and my ambitions and stuff I suppose</p>
+      </div>
+      <div className={styles.links}>
+        <a>Projects</a>
+        <a>Resume</a>
+      </div>
+      <div className={styles.specialties}>
+        <div className={styles.frontendBox}>
+          <h1>Frontend</h1>
+          <p>details</p>
+        </div>
+        <div className={styles.backendBox}>
+          <h1>Backend</h1>
+          <p>details</p>
+        </div>
+        <div className={styles.cicdBox}>
+          <h1>CI/CD</h1>
+          <p>details</p>
+        </div>
+      </div>
+      <div className={styles.skills}>
+          <h1>Skills</h1>
+          <p>figure out how to put a table of skills/technologies from resume here, probably something to do with grids</p>
+          <p>make them decently size with images next to them for each</p>
+          <p>perhaps use favicons or another image importer rather than editing each in individually</p>
+      </div>
     </div>
   );
 }
