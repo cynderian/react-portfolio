98c19691cbb3edaff47bf6bb73bdbe46dc096097
Cynderian <thispersonlikespie@gmail.com>
2025-02-15 22:12:14 -0600

    Basic footer functionality

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,18 +4,20 @@ import styles from "./Footer.module.css";
 export default function Footer() {
     return (
         <>
+          <div className={styles.footer}>
             <div>
-              <h1>Links</h1>
-              <a href="https://www.linkedin.com/in/vivian-tucholski-19226b2b9/">LinkedIn</a>
-              <a href="https://github.com/cynderian">GitHub</a>
+              <h1 className={styles.title}>Links</h1>
+              <a className={styles.link} href="https://www.linkedin.com/in/vivian-tucholski-19226b2b9/">LinkedIn</a>
+              <a className={styles.link} href="https://github.com/cynderian">GitHub</a>
               <a>Resume</a>
             </div>
             <div>
-              <h1>Contact</h1>
+              <h1 className={styles.title}>Contact</h1>
               <h2>vivian.tucholski@gmail.com</h2>
               <h2>678-772-8938</h2>
               <h2>Richardson, TX</h2>
             </div>
-        </>
+          </div>
+      </>
     )
 };
